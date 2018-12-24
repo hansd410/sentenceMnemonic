e183a4413bf223f598d03a5491c57d9d5d5ecabe
ruther <hansd410@gmail.com>
2018-12-24 10:08:31 +0900

    gitignore updated for not updating sh file

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 #python script/train.py --batch-size 10 --test-batch-size 10 --sentence-sewon True --max-context-len 2000
 
 #python script/train.py --gpu 1 --batch-size 10 --test-batch-size 10 --max-context-len 2000 &
-#python script/train.py --gpu 0 --sentence-attention False --batch-size 10 --test-batch-size 10 --max-context-len 2000 &
-python script/train.py --gpu 0 --batch-size 10 --test-batch-size 10 --max-context-len 2000 --answer-hop 1 --align-hop 3 &
+python script/train.py --gpu 0 --sentence-attention False --batch-size 10 --test-batch-size 10 --max-context-len 2000 &
+#python script/train.py --gpu 0 --batch-size 10 --test-batch-size 10 --max-context-len 2000 --answer-hop 1 --align-hop 3 &
 
 #python script/train.py --gpu 1 --pretrained data/model_backup/20181213-b8d8d89e.mdl --sentence-only True &
 
